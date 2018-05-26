c5f30c56939c6a2650d3f1a106474f4a46302a54
vyshor <vyshornehc@gmail.com>
2018-05-26 08:17:22 +0800

    Fixing weird bug on the $ and jQuery, perhaps cacheing

--- LOCALISED.js
+++ LOCALISED.js
@@ -3,6 +3,6 @@ var GITRAW;
 if (!LOCALISED) {
     GITRAW = "https://raw.githubusercontent.com/vyshor/university_expense/master/";
 } else {
-    $.noConflict();
+    $.noConflict(); // seems like a bug that you must upload LOCALISED as 0 everytime otherwise it will know that $ sign doesn't exist
     GITRAW = "";
 }
--- Python/read_kml.py
+++ Python/read_kml.py
@@ -1,7 +1,11 @@
 from fastkml import kml
 import pandas as pd
+from selenium.webdriver import Chrome
+from selenium.webdriver.common.keys import Keys
+import time
+import keyboard
 
-
+WEBDRIVER_PATH = 'uni/chromedriver'
 kml_file = '../version0.04.kml'
 with open(kml_file, 'rt', encoding="utf-8") as myfile:
     doc=myfile.read().encode('utf-8')
@@ -12,14 +16,14 @@ k = kml.KML()
 k.from_string(doc)
 
 features = list(k.features())
-print(len(features))
+# print(len(features))
 
 f2 = list(features[0].features())
-print(len(f2))
-print(f2[0].name)
+# print(len(f2))
+# print(f2[0].name)
 
 f3 = list(f2[0].features())
-print(len(f3))
+# print(len(f3))
 lst = []
 for idx, _ in enumerate(f3):
     # print(f3[idx].name)
@@ -29,12 +33,31 @@ for idx, _ in enumerate(f3):
 PATH_FILE = 'room_types/All.json'
 df = pd.read_json(PATH_FILE)
 df = list(df[0])[:-1]
-print(len(df))
+# print(len(df))
 
 no_location = []
 for location in df:
     if location.upper() not in lst:
-        print(location)
+        # print(location)
         no_location.append(location)
 
-print(len(no_location))
+# print(len(no_location))
+
+URL = 'https://www.gps-coordinates.net/'
+browser = Chrome(WEBDRIVER_PATH)
+browser.get(URL)
+browser.find_element_by_id('address').click()
+time.sleep(5)
+keyboard.press_and_release('ctrl+a, delete')
+keyboard.write('Loyang')
+keyboard.press_and_release('down, enter')
+# keyboard.press('down')
+# keyboard.press('enter')
+
+# for _ in range(20):
+#     browser.find_element_by_id('address').send_keys(Keys.DELETE)
+
+
+
+# browser.find_element_by_id('address').send_keys(Keys.CONTROL + "a" +Keys.DELETE)
+# browser.find_element_by_id('address').clear()
